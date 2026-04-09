26453fcb0d45a35e3421c92f62ec59838e52590d
PevaDaniel <pevadano@gmail.com>
2026-04-09 12:42:46 +0200

    Updated lobby

--- Assets/Scripts/Border.cs
+++ Assets/Scripts/Border.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class Border : MonoBehaviour
 {
+    public float Border1_x = 13;
+    public float Border1_y1 = -2;
+    public float Border1_y2 = 25;
+    public float Border4_x1 = 86;
+    public float Border4_x2 = 113;
+    public float Border4_y1 = 26;
+    public float Border4_y2 = 27;
+    public bool Building1 = true;
+    public bool Building2 = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,21 +20,53 @@ public class Border : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x > 13)
+        if(Building1 == true)
         {
-            transform.position = new Vector3(13, transform.position.y, transform.position.z);
+            Border1();
+        }else
+        {
+            Border2();
         }
-        if(transform.position.x < -13)
+    }
+
+    public void Border1()
+    {
+        Building2 = false;
+        if(transform.position.x > Border1_x)
         {
-            transform.position = new Vector3(-13, transform.position.y, transform.position.z);
+            transform.position = new Vector3(Border1_x, transform.position.y, transform.position.z);
         }
-        if(transform.position.y < -2)
+        if (transform.position.x < -Border1_x)
         {
-            transform.position = new Vector3(transform.position.x, -2, transform.position.z);
+            transform.position = new Vector3(-Border1_x, transform.position.y, transform.position.z);
         }
-        if(transform.position.y > 24)
+        if(transform.position.y < Border1_y1)
         {
-            transform.position = new Vector3(transform.position.x, 24, transform.position.z);
+            transform.position = new Vector3(transform.position.x, Border1_y1, transform.position.z);
+        }
+        if(transform.position.y > Border1_y2)
+        {
+            transform.position = new Vector3(transform.position.x, Border1_y2, transform.position.z);
+        }
+    }
+    public void Border2()
+    {
+        Building1 = false;
+        if(transform.position.x < Border4_x1)
+        {
+            transform.position = new Vector3(Border4_x1, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > Border4_x2)
+        {
+            transform.position = new Vector3(Border4_x2, transform.position.y, transform.position.z);
+        }
+        if(transform.position.y > Border4_y1)
+        {
+            transform.position = new Vector3(transform.position.x, Border4_y1, transform.position.z);
+        }
+        if(transform.position.y < Border4_y2)
+        {
+            transform.position = new Vector3(transform.position.x, Border4_y2, transform.position.z);
         }
     }
 }
